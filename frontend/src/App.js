42d0847d6eeb8f06a038f491fc581794247453bc
rameshsoft <rameshkumar.rigelsoft@gmail.com>
2026-05-20 18:15:39 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import { FileViewerComponent } from "./components/FilePreview";
 
 function App() {
-   const host = window.location.hostname;
-	if (host !== "localhost") {
-		window.location.replace("http://127.0.0.0:8131");
-		return null; // stop rendering
-	}
+//    const host = window.location.hostname;
+// 	if (host !== "localhost") {
+// 		window.location.replace("http://127.0.0.0:8131");
+// 		return null; // stop rendering
+// 	}
   return (
     	<div>
 	    	<Main/>
